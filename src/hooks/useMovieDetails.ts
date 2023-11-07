791eac041143aba47f4e0e5fd11e35be3ba0573b
burnettemk <wafflesxyz@gmail.com>
2023-11-07 15:30:38 -0800

    Use abort controller

--- src/hooks/useMovieDetails.ts
+++ src/hooks/useMovieDetails.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { headers } from "../components/config";
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
 interface Genre {
     id: number;
@@ -13,17 +14,27 @@ interface MovieDetailsResponse {
 }
 
 const useMovieDetails = ( movieId: number) => {
+  const controller = new AbortController();
+
   const [genres, setGenres] = useState<Genre[]>([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
     apiClient.get<MovieDetailsResponse>(`https://api.themoviedb.org/3/movie/${movieId}`,
-      {headers,
-      params: {
-        langauge: 'en-US'
-    }})
+      {
+        headers,
+        params: {
+          langauge: 'en-US'
+        },
+        signal: controller.signal
+      })
     .then(res => setGenres(res.data.genres))
-    .catch(err => setError(err.message))
+    .catch(err => {
+      if (err instanceof CanceledError) return;
+      setError(err.message);
+    })
+
+    return () => controller.abort();
   }, [])
 
   return {genres, error}
