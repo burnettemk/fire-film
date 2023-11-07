af2b35b5a60f9da377cd64cdc609dd67c8bf9033
burnettemk <wafflesxyz@gmail.com>
2023-11-07 15:59:17 -0800

    Add card container

---
+++ src/components/MovieCardContainer.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const MovieCardContainer = () => {
+  return (
+    <Box width='300' borderRadius={10} overflow='hidden'>
+
+    </Box>
+  )
+}
+
+export default MovieCardContainer
