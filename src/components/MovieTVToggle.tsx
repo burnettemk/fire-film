6ddeb389c11077fe0eea8055175126fb4964980c
burnettemk <wafflesxyz@gmail.com>
2023-11-14 13:55:13 -0800

    Setup Navbar

---
+++ src/components/MovieTVToggle.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { HStack, Icon, Text, VStack } from '@chakra-ui/react'
+import { PiArrowBendDownRightBold, PiArrowBendUpLeftBold } from 'react-icons/pi'
+
+const MovieTVToggle = () => {
+  let a = 5;
+  const [isMoviesSelected, setIsMoviesSelected] = useState(true);
+
+  return (
+    <>
+        <VStack color='gray.600'
+            onClick={() => {setIsMoviesSelected(!isMoviesSelected); console.log(isMoviesSelected)}}>
+            <HStack>
+                <Text 
+                  color={isMoviesSelected ? 'gray.400' : ''} 
+                  transition="color 0.25s ease-out"
+                  userSelect='none'
+                >
+                  Movies
+                </Text>
+                <Icon as={PiArrowBendUpLeftBold}/>
+            </HStack>
+            <HStack marginLeft={7} marginTop={-3}>
+                <Icon as={PiArrowBendDownRightBold}/>
+                <Text 
+                  color={isMoviesSelected ? '' : 'gray.400'} 
+                  transition="color 0.25s ease-out"
+                  userSelect='none'
+                  paddingLeft={3}
+                >
+                  TV
+                </Text>
+            </HStack>
+        </VStack>
+    </>
+  )
+}
+
+export default MovieTVToggle
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import { Box, Flex } from '@chakra-ui/react'
+import MovieTVToggle from './MovieTVToggle'
+import SearchInput from './SearchInput'
+
+const NavBar = () => {
+  return (
+    <>
+      <Flex padding={2} mx={3}>
+        <Box 
+            w='40px' 
+            h='40px' 
+            bgGradient='linear(to-br, #FF4500, #ff0000, #fa4d0a)'
+            borderRadius='full'
+            paddingX={3} 
+            paddingY={2}
+            mx={2}
+            color='maroon'
+            boxShadow='xl'
+        >
+            FF
+        </Box>
+        <Box flexGrow={1}>
+          <SearchInput />
+        </Box>
+        <Box mx={2}>
+          <MovieTVToggle />
+        </Box>
+      </Flex>
+    </>
+  )
+}
+
+export default NavBar
