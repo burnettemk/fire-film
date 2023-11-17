4c282385dce3b44e46b93daeb134764d90a0eb94
burnettemk <wafflesxyz@gmail.com>
2023-11-17 15:22:13 -0800

    Remove unresponsive style

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,37 +1,33 @@
-import { Box, Flex, HStack, Spacer } from '@chakra-ui/react'
-import AdvancedFilters from './filters/AdvancedFilters'
-import GenreSelection from './filters/GenreSelection'
-import ListSelection from './filters/ListSelection'
-import RatingSelection from './filters/RatingSelection'
+import { Box, Flex, HStack, Spacer } from "@chakra-ui/react";
+import AdvancedFilters from "./filters/AdvancedFilters";
+import GenreSelection from "./filters/GenreSelection";
+import ListSelection from "./filters/ListSelection";
+import RatingSelection from "./filters/RatingSelection";
 
 const Filters = () => {
   return (
     <>
-      <Box 
-        bg='gray.700' 
-        px='15px' 
-        py='10px' 
-        width='fit-content' 
-        mx='auto'
+      <Box
+        bg="gray.700"
+        px="15px"
+        py="10px"
+        width="fit-content"
+        mx="auto"
         borderRadius={13}
       >
-        <HStack 
-          justifyContent='space-around' 
-          spacing='50px'
-          px='20px'
-        >
+        <HStack justifyContent="space-around" spacing={3} px={{ base: "20px" }}>
           <GenreSelection />
           <RatingSelection />
           <ListSelection />
         </HStack>
-        <Flex mt='10px'>
+        <Flex mt="10px">
           <Spacer />
           <AdvancedFilters />
           <Spacer />
         </Flex>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Filters
+export default Filters;
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,4 @@
-const width = 400;
+const width = 342;
 
 const getCroppedImageUrl = (url: string) => {
   const target = "p/";
