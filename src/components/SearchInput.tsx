c74e5d3fd3c280fe6fee3025282356c2de81f234
burnettemk <wafflesxyz@gmail.com>
2024-01-04 11:49:29 -0800

    Update SearchInput.tsx

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,28 +4,28 @@ import { BsSearch } from "react-icons/bs";
 // import useGameQueryStore from "../store";
 // import { useNavigate } from "react-router-dom";
 
-const SearchInput = ({ style }:  {style?: React.CSSProperties}) => {
+const SearchInput = ({ style }: { style?: React.CSSProperties }) => {
   const ref = useRef<HTMLInputElement>(null);
-//   const setSearchText = useGameQueryStore((s) => s.setSearchText);
-//   const navigate = useNavigate();
+  //   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  //   const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) {
-            ref.current.value = ''
-        //   setSearchText(ref.current.value);
-        //   navigate("/");
+          ref.current.value = "";
+          //   setSearchText(ref.current.value);
+          //   navigate("/");
         }
       }}
     >
-      <InputGroup style={{flexGrow: 1}}>
+      <InputGroup style={{ flexGrow: 1 }}>
         <InputLeftElement children={<BsSearch />} />
         <Input
           ref={ref}
           borderRadius={20}
-          placeholder="Search games..."
+          placeholder="Search content..."
           variant="filled"
         />
       </InputGroup>
