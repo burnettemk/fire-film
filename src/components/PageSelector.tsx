22a9e08f43e1ae4f6d5374caa184408572d3dd17
burnettemk <wafflesxyz@gmail.com>
2024-01-22 02:58:29 -0800

    Prettier

--- src/components/PageSelector.tsx
+++ src/components/PageSelector.tsx
@@ -1,21 +1,29 @@
-import { Button, Flex, HStack, Icon, Image, Spacer, Text } from '@chakra-ui/react';
-import React from 'react'
+import {
+  Button,
+  Flex,
+  HStack,
+  Icon,
+  Image,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const PageSelector = () => {
   return (
-    <Flex pb='15px'>
+    <Flex pb="15px">
       <Spacer />
-      <Button bg='none' height='25px'>
-        <Icon as={IoIosArrowBack}/>
+      <Button bg="none" height="25px" onClick={() => console.log("prev page")}>
+        <Icon as={IoIosArrowBack} />
       </Button>
-      <Text px='25px'>1</Text>
-      <Button bg='none' height='25px'>
-        <Icon as={IoIosArrowForward}/>
+      <Text px="25px">1</Text>
+      <Button bg="none" height="25px" onClick={() => console.log("next page")}>
+        <Icon as={IoIosArrowForward} />
       </Button>
       <Spacer />
     </Flex>
-  )
-}
+  );
+};
 
-export default PageSelector
+export default PageSelector;
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -3,39 +3,40 @@ import { useEffect, useState } from "react";
 import { headers } from "../services/config";
 import apiClient from "../services/api-client";
 import genres from "../data/genres";
-  
+
 interface FetchResponse<T> {
-    results: T[];
-    genres: T[];
+  results: T[];
 }
 
-const useData = <T>(endpoint : string, params : {}) => {
+const useData = <T>(endpoint: string, params: {}) => {
   const [data, setData] = useState<T[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
     setIsLoading(true);
-    apiClient.get<FetchResponse<T>>(endpoint
-    , { headers,
+    apiClient
+      .get<FetchResponse<T>>(endpoint, {
+        headers,
         params,
-        signal: controller.signal})
-    .then((res) => {
-      setData(res.data.genres);
-      setIsLoading(false);
-    })
-    .catch(err => {
-      if (err instanceof CanceledError) return;
-      setError(err.message);
-      setIsLoading(false);
-    })
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setData(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
 
     return () => controller.abort();
-  }, [])
+  }, []);
 
-  return {data, error, isLoading}
-}
+  return { data, error, isLoading };
+};
 
 export default useData;
