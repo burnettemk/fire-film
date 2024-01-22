58d40f0552bd61ce0c02c9567b08d34bee3fcde3
burnettemk <wafflesxyz@gmail.com>
2024-01-22 03:12:57 -0800

    Error fix - useGenres.ts

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,10 @@
 import useData from "./useData";
 
 export interface Genre {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
-const useGenres = () => useData<Genre>('/genre/movie/list', {langauge: 'en'})
+const useGenres = () => useData<Genre>("/genre/movie/list");
 
 export default useGenres;
